d0127f3651f84ba1cb98d6a784883a013c96b12e
roma <romhedoo@gmail.com>
2019-12-04 19:48:59 +0100

    UML class diagram added

--- fs/settings/base.py
+++ fs/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions', #https://django-extensions.readthedocs.io/en/latest/graph_models.html
     'storages',
     'players',
 ]
